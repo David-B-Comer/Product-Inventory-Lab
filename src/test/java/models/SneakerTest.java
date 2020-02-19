1b4bdefea4353a988e0edc8169153f8bbe679ac1
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-02-19 09:07:15 -0500

    sneaker test

--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,4 +1,19 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SneakerTest {
+
+    private Sneaker testSneaker;
+
+    @Test
+    public void setNameTest() {
+        String expected = "OZWEEGO";
+
+        testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
 }
